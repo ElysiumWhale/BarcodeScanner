77ad824e8f7d3d35af0f51a312e8dc1c90bc9e68
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-03-10 01:40:46 +0400

    Rework ScannerVC

--- Sources/Views/UIKit/ScannerVC.swift
+++ Sources/Views/UIKit/ScannerVC.swift
@@ -5,6 +5,15 @@ enum CameraError: Error {
     case invalidDeviceInput(alert: AlertItem = AlertItem.invalidDeviceInput)
     case invalidScannedValue(alert: AlertItem = AlertItem.invalidScannedValue)
     case notScannedYet(alert: AlertItem = AlertItem.notScannedYet)
+
+    var alertTitle: String {
+        switch self {
+            case .invalidDeviceInput(let alert),
+                 .invalidScannedValue(let alert),
+                 .notScannedYet(let alert):
+                return alert.title
+        }
+    }
 }
 
 protocol ScannerVCDelegate: AnyObject {
@@ -17,9 +26,9 @@ final class ScannerVC: UIViewController {
     var previewLayer: AVCaptureVideoPreviewLayer?
 
     weak var scannerDelegate: ScannerVCDelegate?
-    
-    private var haveAccess: Bool = false
-    
+
+    private var hasAccess: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCaptureSession()
@@ -35,66 +44,46 @@ final class ScannerVC: UIViewController {
 
         layer.frame = view.layer.bounds
     }
-    
-    init(scannerDelegate: ScannerVCDelegate) {
-        super.init(nibName: nil, bundle: nil)
-        self.scannerDelegate = scannerDelegate
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
     private func setupCaptureSession() {
-        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
-            scannerDelegate?.didFind(error: .invalidDeviceInput())
-            return
-        }
-        
-        let videoInput: AVCaptureDeviceInput
-        
-        do {
-            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
-        } catch {
-            scannerDelegate?.didFind(error: .invalidDeviceInput())
-            return
-        }
-        
-        if captureSession.canAddInput(videoInput) {
-            captureSession.addInput(videoInput)
-        } else {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video),
+              let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice),
+              captureSession.canAddInput(videoInput) else {
             scannerDelegate?.didFind(error: .invalidDeviceInput())
             return
         }
 
+        captureSession.addInput(videoInput)
         let metaDataOutput = AVCaptureMetadataOutput()
-        
-        if captureSession.canAddOutput(metaDataOutput) {
-            captureSession.addOutput(metaDataOutput)
-            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
-        } else {
+
+        guard captureSession.canAddOutput(metaDataOutput) else {
             scannerDelegate?.didFind(error: .invalidDeviceInput())
             return
         }
+
+        captureSession.addOutput(metaDataOutput)
+        metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
 
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer!.videoGravity = .resizeAspectFill
         view.layer.addSublayer(previewLayer!)
-        
-        haveAccess = true
+
+        hasAccess = true
         captureSession.startRunning()
     }
 
     func startScan() {
-        if !captureSession.isRunning && haveAccess {
+        if !captureSession.isRunning && hasAccess {
             captureSession.startRunning()
         }
     }
 }
 
 extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
-    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    func metadataOutput(_ output: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
         guard let object = metadataObjects.first,
               let readableObject = object as? AVMetadataMachineReadableCodeObject,
               let barcode = readableObject.stringValue else {
--- Sources/Views/UIKit/ScannerView.swift
+++ Sources/Views/UIKit/ScannerView.swift
@@ -1,11 +1,16 @@
 import SwiftUI
 
 struct ScannerView: UIViewControllerRepresentable {
-    @Binding var result: Result<String, CameraError>
-    @Binding var isScanning: Bool
-    
+    @Binding
+    var result: Result<String, CameraError>
+
+    @Binding
+    var isScanning: Bool
+
     func makeUIViewController(context: Context) -> ScannerVC {
-        ScannerVC(scannerDelegate: context.coordinator)
+        let vc = ScannerVC()
+        vc.scannerDelegate = context.coordinator
+        return vc
     }
 
     func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {
